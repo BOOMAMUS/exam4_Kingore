b30e177a0541b14e37bd922e3364ee8387d73a49
Drennon <dk1008065@otc.edu>
2025-12-08 20:16:48 -0600

    Commit With Json file

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
+// npm init -y
 const express = require('express');
 const app = express();
 
 app.use(express.json());
-
+const localhost = 'http://localhost:3000/pets'
 const server = 3000;
-app.listen(server, () => {
-    console.log(`Server is running on port http://localhost:${server}`);
+app.listen(localhost, () => {
+    console.log(`Server is running on port http://localhost:${server}/pets`);
     console.log('Hello World!');
 })
 
@@ -13,8 +14,9 @@ app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
-const pets 
-
+app.get('/pets', (req, res) => {
+    res.json(pets);
+});
 const pets = [
 { id: 1, type: "dog", name: "Diesel" },
 { id: 2, type: "cat", name: "Milo" },
